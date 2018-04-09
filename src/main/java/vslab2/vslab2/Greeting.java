6feea779bb63f5eab9c02df7dda6a160e3a3c6fa
Gil Biren <gil@biren.eu>
2018-04-09 15:01:47 +0200

    init

---
+++ src/main/java/vslab2/vslab2/Greeting.java
@@ -0,0 +1,32 @@
+package vslab2.vslab2;
+
+public class Greeting {
+
+    private String name;
+    private String content;
+    private boolean bootstrap;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isBootstrap() {
+        return bootstrap;
+    }
+
+    public void setBootstrap(boolean bootstrap) {
+        this.bootstrap = bootstrap;
+    }
+}
---
+++ src/main/java/vslab2/vslab2/SpringWebDemoApplication.java
@@ -0,0 +1,12 @@
+package vslab2.vslab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebDemoApplication.class, args);
+    }
+}
