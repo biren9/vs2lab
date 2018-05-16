96cc62b3661d042e1ad17d37ae9e6272eba918aa
Gabriel Testault <gabriel@testault.com>
2018-05-16 12:26:40 +0200

    getMessage api

--- src/main/java/vslab2/vslab2/controller/TweetWallController.java
+++ src/main/java/vslab2/vslab2/controller/TweetWallController.java
@@ -1,14 +1,20 @@
 package vslab2.vslab2.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class TweetWallController {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
     @RequestMapping(value = "/tweetWall")
-    public String greetingSubmit() {
+    public String greetingSubmit(HttpSession session) {
+        log.info((String) session.getAttribute("test"));
         return  "tweetWall";
     }
 }
