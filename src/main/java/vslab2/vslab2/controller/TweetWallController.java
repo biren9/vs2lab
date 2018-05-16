a06cb4b71c1ab8b0ddfb60d9db838b925dbc6ce7
Gil Biren <gilbiren@gmail.com>
2018-05-16 12:45:45 +0200

    fetch json from DB

--- src/main/java/vslab2/vslab2/controller/TweetWallController.java
+++ src/main/java/vslab2/vslab2/controller/TweetWallController.java
@@ -2,6 +2,8 @@ package vslab2.vslab2.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,11 +11,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import vslab2.vslab2.service.ManageUsersService;
 import javax.servlet.http.HttpSession;
+import java.util.*;
 
 @Controller
 public class TweetWallController {
+
+    @Autowired
+    ManageUsersService service;
     ObjectMapper mapper = new ObjectMapper();
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
@@ -27,6 +33,10 @@ public class TweetWallController {
     @ResponseBody
     public String getTweets() {
         JsonNode node = mapper.createObjectNode();
+        List<String> messages = service.getMessage("pknp", 0, 10);
+        for(String message : messages) {
+            ((ObjectNode) node).put(message, message);
+        }
         return node.toString();
     }
 }
