1a31244908d582e5965ee92dafa6e09708b072e1
Gil Biren <gilbiren@gmail.com>
2018-05-30 10:19:35 +0200

    Layout appending messages same as thymeleaf

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -2,21 +2,22 @@ $(document).ready(function() {
     let pageCount = 1;
     let allTweetsLoaded = false;
     let requestDone = new Array();
-    $(window).scroll(function() {
-        if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
-            if(requestDone[pageCount] === undefined && !allTweetsLoaded) {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+            if (requestDone[pageCount] === undefined && !allTweetsLoaded) {
                 let user = getCookie("client_username");
                 if (window.location.pathname.split("/").pop() === "global") {
                     user = "global";
                 }
                 requestDone[pageCount] = true;
-                $.get("/api/messages/" + user + "/" +pageCount, function (data) {
+                $.get("/api/messages/" + user + "/" + pageCount, function (data) {
                     pageCount += 1;
-                    if(data.messages.length > 0) {
+                    if (data.messages.length > 0) {
                         $.each(data.messages, function (key, value) {
                             var obj = JSON.parse(value);
                             console.log(obj);
-                            if (data.subs.includes(obj.author)) {
+                            var currentUser = getCookie("client_username");
+                            if(currentUser === obj.author) {
                                 $("#tweetwall").append("<div class=\"card col-md-10 mt-5 offset-md-1\">\n" +
                                     "                    <div class=\"card-header\">\n" +
                                     "                        Bitt\n" +
@@ -26,7 +27,20 @@ $(document).ready(function() {
                                     "                        <h5 class=\"card-title\">" + obj.text + "</h5>\n" +
                                     "                    </div>\n" +
                                     "                    <div class=\"card-footer\">\n" +
-                                    "                        <a href=\"#\" class=\"btn btn-info\">- " + obj.author + "</a>\n" +
+                                    "                    </div>\n" +
+                                    "                </div>");
+                            }
+                            else if (data.subs.includes(obj.author)) {
+                                $("#tweetwall").append("<div class=\"card col-md-10 mt-5 offset-md-1\">\n" +
+                                    "                    <div class=\"card-header\">\n" +
+                                    "                        Bitt\n" +
+                                    "                        <span class=\"right small\">" + obj.timestamp + "</span>\n" +
+                                    "                    </div>\n" +
+                                    "                    <div class=\"card-body\">\n" +
+                                    "                        <h5 class=\"card-title\">" + obj.text + "</h5>\n" +
+                                    "                    </div>\n" +
+                                    "                    <div class=\"card-footer\">\n" +
+                                    "                        <button class='btn btn-info followButton'>- " + obj.author + "</button>\n" +
                                     "                    </div>\n" +
                                     "                </div>");
                             } else {
@@ -39,12 +53,13 @@ $(document).ready(function() {
                                     "                        <h5 class=\"card-title\">" + obj.text + "</h5>\n" +
                                     "                    </div>\n" +
                                     "                    <div class=\"card-footer\">\n" +
-                                    "                        <a href=\"#\" class=\"btn btn-success\">+ " + obj.author + "</a>\n" +
+                                    "                        <button class='btn btn-success followButton'>+ " + obj.author + "</button>\n" +
                                     "                    </div>\n" +
                                     "                </div>");
                             }
 
                         });
+                        followListener();
                     }
                     else {
                         allTweetsLoaded = true;
@@ -54,45 +69,47 @@ $(document).ready(function() {
             }
         }
     });
-    $(".post-message").click(function(e) {
+    $(".post-message").click(function (e) {
         e.preventDefault();
         let author = getCookie("client_username");
         let bitter = {
-            "timestamp" : null,
-            "author" : author,
-            "text" : $('.modal-input').val()
+            "timestamp": null,
+            "author": author,
+            "text": $('.modal-input').val()
         };
         $('#exampleModal').modal('hide');
         $.ajax("/api/messages/" + author,
             {
-                "data" : JSON.stringify(bitter),
-                "method" : "POST",
-                "success" : function(data, _) {
+                "data": JSON.stringify(bitter),
+                "method": "POST",
+                "success": function (data, _) {
                     $('.modal-input').val("");
                     location.reload();
 
                 },
-                "error": function(_, status, error ) {
+                "error": function (_, status, error) {
                     console.log(error)
                 },
-                "contentType" : "application/json"
+                "contentType": "application/json"
             });
     });
-    $(".followButton").click(function(e) {
-        e.preventDefault();
-        var button = $(this);
-        let buttonText = $(this).text();
-        let extract = buttonText.lastIndexOf(' ');
-        let userToFollow = buttonText.substring(extract + 1);
-        let apiPath = buttonText.substring(0, 1) === '+'? 'follow' : 'unfollow';
-        let user = getCookie("client_username");
-        let targetUrl = "/api/"+apiPath+"/" + user;
-        $.ajax(targetUrl, {
-                "data" : JSON.stringify({follow: userToFollow}),
-                "method" : "POST",
-                "success" : function(data) {
+
+    function followListener() {
+        $(".followButton").click(function (e) {
+            e.preventDefault();
+            var button = $(this);
+            let buttonText = $(this).text();
+            let extract = buttonText.lastIndexOf(' ');
+            let userToFollow = buttonText.substring(extract + 1);
+            let apiPath = buttonText.substring(0, 1) === '+' ? 'follow' : 'unfollow';
+            let user = getCookie("client_username");
+            let targetUrl = "/api/" + apiPath + "/" + user;
+            $.ajax(targetUrl, {
+                "data": JSON.stringify({follow: userToFollow}),
+                "method": "POST",
+                "success": function (data) {
                     console.log(apiPath);
-                    if(apiPath === "follow") {
+                    if (apiPath === "follow") {
                         button.removeClass("btn-success");
                         button.addClass("btn-info");
                         button.text("- " + userToFollow);
@@ -103,12 +120,14 @@ $(document).ready(function() {
                         button.text("+ " + userToFollow);
                     }
                 },
-                "error": function(_, status, error ) {
+                "error": function (_, status, error) {
                     console.log(error);
                 },
-                "contentType" : "application/json"
+                "contentType": "application/json"
+            });
         });
-    });
+    }
+    followListener();
     $(".userSearchInput").keydown(function() {
         let userString = $(this).val();
         let dataList = $("#searchresults");
